eb64daee91237a1a1a863ff683227d48e291d948
don1502 <dongomez1502@gmail.com>
2025-12-28 15:39:08 +0530

    Linear search basic

---
+++ src/LinearSearch.java
@@ -0,0 +1,29 @@
+
+// This is the basic linear Search
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.println("Enter the element to be searched: ");
+        int target = input.nextInt();
+        linearSearch(arr, target);
+    }
+
+    static void linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Element found");
+                return;
+            }
+        }
+        System.out.println("Element not found");
+    }
+}
