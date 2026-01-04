f2af59432711065fde2d0561cec7dd41d1e46276
don1502 <dongomez1502@gmail.com>
2026-01-04 20:47:04 +0530

    Leetcode problem 153. Find Minimum in Rotated Sorted Array...

---
+++ src/Binarysearch/MinInRotatedArray.java
@@ -0,0 +1,60 @@
+package Binarysearch;
+
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
+// Leetcode problem 153. Find Minimum in Rotated Sorted Array
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MinInRotatedArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        int result = searchMin(arr);
+        System.out.println("The minimum value is " + result );
+    }
+
+    // Main leetcode function....
+
+    static int searchMin(int [] arr)
+    {int pivot = pivotElement(arr);
+
+    // if you did not find a pivot, it means the array doesnot rotated so do normal binary search
+        if (pivot == -1){
+        return arr[0];
+    }
+    // if pivot found
+        return arr[pivot + 1];
+
+}
+static int pivotElement(int[] arr){
+    int low = 0;
+    int high = arr.length - 1;
+    int mid = 0;
+    while(low <= high){
+        mid = low + (high - low) / 2;
+
+        // there are 4 cases to find the pivot element
+
+        if ( mid < high && arr[mid] > arr[mid+1]){ // we check that mid<high because at one point mid will
+            // point high and if mid+1 happens it will give arrayoutofbound
+            return mid;
+        }
+        if (mid > low && arr[mid] < arr[mid-1]){
+            return mid-1;
+        }
+        if (arr[low] >= arr[mid]){
+            high = mid - 1;
+        }else {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+}
