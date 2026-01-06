3e7764a6241573dbbda3acc2cc64e8b74d1f81d3
don1502 <dongomez1502@gmail.com>
2026-01-06 22:44:04 +0530

    Sorting, Bubble sort finished...

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,46 @@
+package Sorting;
+
+// Bubble sort is technique which compare adjustent element and swap them
+// It is also called as Sinking sort or exchange sort.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        bubblesort(arr);
+
+        System.out.println("Sorted array is " + Arrays.toString(arr));
+    }
+
+    static void bubblesort(int[] arr){
+        // run step n-1 times
+        boolean swapped;
+
+        for (int i = 0; i < arr.length; i++) {
+            // for each step max item will come at the last index...
+            for(int j = 1; j <= arr.length - i - 1; j++){
+                // Swap if the item is smaller than the previous item...
+                if (arr[j] < arr[j-1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            // if you did not swap for a particular value of i, it means array is already sorted hence end function
+            if (!swapped){
+                break;
+            }
+        }
+    }
+}
