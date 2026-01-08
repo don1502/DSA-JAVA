574125d5c63409f6b466e4309d2b3b3d554d8b6b
don1502 <dongomez1502@gmail.com>
2026-01-08 12:40:59 +0530

    Cyclic sorting implementation....

---
+++ src/Sorting/cyclicSorting.java
@@ -0,0 +1,33 @@
+package Sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class cyclicSorting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        sort(arr);
+        System.out.print("The sorted array " + Arrays.toString(arr));
+    }
+
+    static void sort(int [] arr){
+        int i = 0;
+        while(i< arr.length){
+            int correct = arr[i]-1;
+            if(arr[i] != arr[correct]){
+                swapping(arr, i, correct);
+            }else i++;
+        }
+    }
+    static void swapping(int [] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
