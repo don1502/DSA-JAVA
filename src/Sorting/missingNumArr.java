c2eba7931caaccb52635a4cdcbc2ad67d3b9280b
don1502 <dongomez1502@gmail.com>
2026-01-08 12:18:09 +0530

    Missing number in the array ...

--- src/Sorting/missingNumArr.java
+++ src/Sorting/missingNumArr.java
@@ -1,4 +1,29 @@
 package Sorting;
 
+import java.util.Scanner;
+
 public class missingNumArr {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.println("The missing value in array is "+ missingNumber(arr));
+    }
+    static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int total = n*(n+1)/2;
+        return (total - sumOfArray(nums));
+    }
+
+    static int sumOfArray(int [] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
 }
