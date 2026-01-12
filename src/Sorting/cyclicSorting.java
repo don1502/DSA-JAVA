135f290aab30a2bf2344f9c4d30503c16988f831
don1502 <dongomez1502@gmail.com>
2026-01-12 22:15:14 +0530

    Small changes in cyclic sort..

--- src/Sorting/cyclicSorting.java
+++ src/Sorting/cyclicSorting.java
@@ -20,7 +20,7 @@ public class cyclicSorting {
         int i = 0;
         while(i< arr.length){
             int correct = arr[i]-1;
-            if(arr[i] != arr[correct]){
+            if(arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]){
                 swapping(arr, i, correct);
             }else i++;
         }
