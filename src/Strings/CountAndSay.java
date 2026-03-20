d8f66278498222ae7a1702ca97fc6da724e81dde
don1502 <dongomez1502@gmail.com>
2026-03-20 15:11:51 +0530

    Leetcode problem 38. Count and Say...

---
+++ src/Strings/CountAndSay.java
@@ -0,0 +1,34 @@
+package Strings;
+
+// https://leetcode.com/problems/count-and-say/
+// Leetcode problem 38. Count and Say
+
+import java.util.*;
+
+public class CountAndSay {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n = input.nextInt();
+        System.out.println("Count and say Run-Length Encoding: " + countAndSay(n));
+    }
+
+    // Main leetcode function...
+
+    static String countAndSay(int n) {
+        String curr = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int j = 0;
+            while (j < curr.length()) {
+                char ch = curr.charAt(j);
+                int count = 0;
+                while (j < curr.length() && curr.charAt(j) == ch) { j++; count++; }
+                sb.append(count).append(ch);
+            }
+            curr = sb.toString();
+        }
+        return curr;
+    }
+
+}
