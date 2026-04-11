94181e1ec68295ec93f1c2ad1cc0032851029294
don1502 <dongomez1502@gmail.com>
2026-04-11 14:19:50 +0530

    Fibonacci using DP level1 and tabulation

--- src/DynamicProgramming/Fibonacci.java
+++ src/DynamicProgramming/Fibonacci.java
@@ -1,7 +1,6 @@
 package DynamicProgramming;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 // This is the starting of memoization and Dynamic Programming....
 
@@ -10,7 +9,11 @@ public class Fibonacci {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number to calculate fibonacci: ");
         int n = input.nextInt();
+        long[] dp = new long[n + 1];
+        Arrays.fill(dp, -1);
         System.out.println("The fibonacci of the number " + n + " is " + fib(n));
+        System.out.println("The fibonacci of the number " + n + " is " + fibdp(n, dp));
+        System.out.println("The fibonacci of the number " + n + " is " + fibdp2(n, dp));
     }
 
     // Main function...
@@ -27,6 +30,30 @@ public class Fibonacci {
         long result = fib(a - 1) + fib(a - 2);
         memo.put(a, result);
         return result;
+    }
+
+    // if(a <= 2) return 1;
+    // if(a <= 1) return a;
+    // Both are same only
+
+    // Using level 1 DP
+    static long fibdp(int a, long[] dp){
+        if (dp[a] != -1) return dp[a];
+
+        if(a <= 1) return a;
+
+        return dp[a] = fibdp(a - 1, dp) + fibdp(a - 2, dp);
+    }
+
+    // Using tabulation or level 2 dp
+    static long fibdp2(int a, long[] dp){
+        // Updating both base cases into dp
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i < a; i++) dp[i] = dp[i-1] + dp[i-2];
+        // Why i = 2, because dp[0] and dp[1] already updated;
+        return dp[a];
     }
 
 }
