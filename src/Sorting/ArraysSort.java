b3828972170f8a6200c63ce74b3214c802ed9b8c
mugunthan7eagles <mugunthan@the7eagles.com>
2026-06-04 11:47:31 +0530

    Arrays

---
+++ src/Sorting/ArraysSort.java
@@ -0,0 +1,11 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class ArraysSort {
+    public static void main(String[] args) {
+        int [] arr = {3, 9, 1, 6, 7};
+        Arrays.sort(arr); // ----- type 1
+        System.out.println(Arrays.toString(arr));
+    }
+}
