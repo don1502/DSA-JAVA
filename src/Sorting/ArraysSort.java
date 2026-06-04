8e04df0158514499f15fc45a8a4a731a0916ab9a
don1502 <dongomez1502@gmail.com>
2026-06-04 14:19:05 +0530

    Small updation

--- src/Sorting/ArraysSort.java
+++ src/Sorting/ArraysSort.java
@@ -7,5 +7,16 @@ public class ArraysSort {
         int [] arr = {3, 9, 1, 6, 7};
         Arrays.sort(arr); // ----- type 1
         System.out.println(Arrays.toString(arr));
+
+        int [][] arr1 = {
+                 {1,9,3}
+                ,{6,2,2}
+                ,{4,4,1}};
+        Arrays.sort(arr1, (a, b) -> a[1] - b[1]);
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(Arrays.toString(arr1[i]));
+        }
+
     }
 }
