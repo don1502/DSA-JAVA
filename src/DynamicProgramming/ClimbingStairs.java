f3d944d886ce2a0893940086342d41a1ee2aa234
don1502 <dongomez1502@gmail.com>
2026-06-08 09:59:47 +0530

    Space optimization for climbing stairs problem.....

--- src/DynamicProgramming/ClimbingStairs.java
+++ src/DynamicProgramming/ClimbingStairs.java
@@ -10,8 +10,29 @@ public class ClimbingStairs {
         System.out.println(climbingStrirs(n));
     }
 
-    // Stairs can be climbed by two ways either by single steps or double steps.
 
+    // Space optimised solution for climbing stairs....
+
+    // public int climbStairs(int n) {
+
+    //     if (n == 1) return 1;
+    //     if (n == 2) return 2;
+
+    //     int first = 1;
+    //     int second = 2;
+    //     int result = 0;
+
+    //     for (int i = 3; i <= n; i++) {
+    //         result = first + second;
+    //         first = second;
+    //         second = result;
+    //     }
+
+    //     return result;
+    // }
+
+
+    // Stairs can be climbed by two ways either by single steps or double steps.
     // This is a solution using dynamic programming bottom-up approach
 
     static int climbingStrirs(int n) {
