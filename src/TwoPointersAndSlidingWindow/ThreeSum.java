74a19fb1c2638293094e200ef97e579cde04b362
don1502 <dongomez1502@gmail.com>
2026-06-12 14:02:20 +0530

    Leetcode problem Scanner input = new Scanner(System.in);

---
+++ src/TwoPointersAndSlidingWindow/ThreeSum.java
@@ -0,0 +1,53 @@
+package TwoPointersAndSlidingWindow;
+
+// https://leetcode.com/problems/3sum/
+// Leetcode problem 15. 3Sum
+
+import java.util.*;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int [] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = input.nextInt();
+        }
+        System.out.println(threeSum(coins));
+    }
+
+    // Main leetcode function...
+
+    static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
