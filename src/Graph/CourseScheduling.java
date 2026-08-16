bde7cadfb9ccbbeca05fcfc39277921513726b2c
don1502 <dongomez1502@gmail.com>
2026-08-16 13:48:50 +0530

    Leetcode problem 207. Course Schedule....

---
+++ src/Graph/CourseScheduling.java
@@ -0,0 +1,42 @@
+package Graph;
+
+// https://leetcode.com/problems/course-schedule/
+// Leetcode problem 207. Course Schedule
+
+import java.util.*;
+
+public class CourseScheduling {
+    public static void main(String[] args) {
+
+    }
+
+    // Main leetcode function.....
+
+    static boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+
+        for (int[] pre : prerequisites)
+            adj.get(pre[1]).add(pre[0]);
+
+        boolean[] vis = new boolean[numCourses];
+        boolean[] path = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; i++)
+            if (!vis[i] && dfs(i, adj, vis, path)) return false;
+
+        return true;
+    }
+
+    static boolean dfs(int node, List<List<Integer>> adj, boolean[] vis, boolean[] path) {
+        vis[node] = path[node] = true;
+
+        for (int next : adj.get(node))
+            if (!vis[next] && dfs(next, adj, vis, path)) return true;
+            else if (path[next]) return true;
+
+        path[node] = false;
+        return false;
+    }
+
+}
